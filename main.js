c72b4251a12d47be732757aef88a7a254c06f21a
Dong Ho, Shin <31787715+TLparche@users.noreply.github.com>
2025-03-18 12:29:48 +0900

    temp

--- main.js
+++ main.js
@@ -1,30 +1,32 @@
-const { app, BrowserWindow } = require('electron/main')
-const path = require('node:path')
+const { app, BrowserWindow } = require('electron');
 
-function createWindow () {
+function createWindow() {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 300,
+        height: 100,
+        transparent: true,
+        frame: false,
+        alwaysOnTop: true,
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
+            nodeIntegration: true,
+            contextIsolation: false
         }
-    })
+    });
 
-    win.loadFile('index.html')
+    win.loadFile('index.html');
+    win.setIgnoreMouseEvents(true);
 }
 
-app.whenReady().then(() => {
-    createWindow()
-
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow()
-        }
-    })
-})
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
-        app.quit()
+        app.quit();
     }
-})
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
